a615491c6e1a57ff8f7ed0e9bf09cc5137297883
David Benson <dmebenson1978@gmail.com>
2017-04-03 20:29:30 +0100

    Setup basic routing
    Install ‘httpdispatcher’
    Add json to server dir
    Add README and devNotes
    Create .gitignore

---
+++ devNotes.md
@@ -0,0 +1,8 @@
+# Start Server
+
+`$ node server/nodeserver.js`
+
+# Access Server
+
+In browser:
+http://localhost:8080/
--- server/nodeserver.js
+++ server/nodeserver.js
@@ -1,14 +1,54 @@
-// Import HTTP module
-var http = require('http');
+// Import modules
+var http           = require('http'),
+    httpdispatcher = require('httpdispatcher'),
+    dispatcher     = new httpdispatcher();
 
 // Define listen port
 const PORT = 8080;
 
+// Set asset dir
+dispatcher.setStatic('assets');
+
+// GET /search
+dispatcher.onGet('/search', function (request, response) {
+    'use strict';
+
+    console.log('Request: ' + request.url);
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Request: ' + request.url);
+});
+
+// POST /results
+dispatcher.onPost('/results', function (request, response) {
+    'use strict';
+
+    console.log('Request: ' + request.url);
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Request: ' + request.url);
+});
+
+dispatcher.onError(function (request, response) {
+    'use strict';
+
+    console.log('No request handler found for ' + request.url);
+
+    response.writeHead(404);
+    response.end('Requested page not found');
+});
+
 // Request handler
 function handleRequest(request, response) {
     'use strict';
-
-    response.end('Path requested: ' + request.url);
+    try {
+        // Log request
+        console.log(request.url);
+        // Disptach
+        dispatcher.dispatch(request, response);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 // Create server
@@ -19,5 +59,5 @@ server.listen(PORT, function () {
     'use strict';
 
     //Callback triggered when server listening.
-    console.log("Server listening on: http://localhost:%s", PORT);
+    console.log('Server listening on: http://localhost:%s', PORT);
 });
